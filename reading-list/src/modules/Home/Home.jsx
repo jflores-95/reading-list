f517d1671d3de6bf10b150d8fea29c2eedb337f9
jflores-95 <jfloreesc@gmail.com>
2023-09-23 13:25:00 -0600

    Feat: Page setup

---
+++ reading-list/src/modules/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import * as Styled from "./Home.styled.js";
+
+export const Home = () => {
+  return (
+    <>
+      <header>
+        <Styled.Title>The Secret Book Club </Styled.Title>
+        <Styled.SubTitle>What are you going to read?</Styled.SubTitle>
+      </header>
+    </>
+  );
+};
---
+++ reading-list/src/modules/Home/Home.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+font-family: 'Mooli', sans-serif;
+`
+
+export const SubTitle = styled.h3`
+font-family: 'Roboto', sans-serif;
+`
